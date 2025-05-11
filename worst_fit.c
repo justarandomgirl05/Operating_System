f3b3b0da1d4c0f3a12d1be20f4e9f585399a2a78
Chaitanyaa Deokar <blossomcherry24766@gmail.com>
2025-05-11 23:29:53 +0530

    Add files via upload

---
+++ worst_fit.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int nb, np, i, j, index;
+
+    printf("Enter number of blocks: ");
+    scanf("%d", &nb);
+    printf("Enter number of processes: ");
+    scanf("%d", &np);
+
+    // int b[nb], bf[nb], p[np], frag[np], bn[np];
+    int *b = (int *)malloc(nb * sizeof(int));
+    int *bf = (int *)malloc(nb * sizeof(int)); // block size, 0 = free, 1 = allocated
+    int *p = (int *)malloc(np * sizeof(int));
+    int *frag = (int *)malloc(np * sizeof(int));
+    int *bn = (int *)malloc(np * sizeof(int));
+
+    printf("Enter the size of each block: ");
+    for (i = 0; i < nb; i++)
+    {
+        scanf("%d", &b[i]);
+        bf[i] = 0; // Initialize block flags
+    }
+
+    printf("Enter the size of each process: ");
+    for (i = 0; i < np; i++)
+    {
+        scanf("%d", &p[i]);
+        frag[i] = 0; // Initialize fragmentation
+        bn[i] = -1;  // Initialize block number (-1 for unallocated)
+    }
+
+    for (i = 0; i < np; i++)
+    {
+        int largest = 0;
+        index = -1; // Reset index for each process
+
+        for (j = 0; j < nb; j++)
+        {
+            if (bf[j] == 0 && b[j] >= p[i])
+            { // Check if block is free and large enough
+                if (b[j] > largest)
+                { // Find the smallest suitable block
+                    largest = b[j];
+                    index = j;
+                }
+            }
+        }
+
+        if (index != -1)
+        {                             // If a suitable block is found
+            bf[index] = 1;            // Mark the block as allocated
+            bn[i] = index;            // Assign block to process
+            frag[i] = largest - p[i]; // Calculate fragmentation
+        }
+    }
+
+    printf("Worst Fit Algorithm :\n");
+    printf("process No \t process size \t block No \t Block size \t Fragment of process\t \n ");
+    for (i = 0; i < np; i++)
+    {
+        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\t\n", i + 1, p[i], bn[i] + 1, b[bn[i]], frag[i]);
+    }
+    return 0;
+}
